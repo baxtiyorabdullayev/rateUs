162aea098de109d0d49b49188410a76e00ce8faa
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-03-31 02:18:13 +0500

    all html elements creater with js, and some css styles changeed

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 "use strict";
 
-const rateBlock = document.querySelector(".rate_block");
-const point = document.querySelector(".point");
-const button = document.querySelector(".button");
+let body = document.querySelector("body");
+
+let container = document.createElement("div");
+container.className = "container";
+
+let text = document.createElement("h2");
+text.textContent = "Rate us";
+
+let rateBox = document.createElement("div");
+rateBox.className = "rate_box";
+
+let rateBlock = document.createElement("div");
+rateBlock.className = "rate_block";
+
+let yellowImage = document.createElement("img");
+yellowImage.src = "./rate.png";
+
+let point = document.createElement("input");
+point.type = "range";
+point.className = "point";
+point.value = 0;
+point.min = 1;
+point.max = 100;
 
 function change() {
   let num = +point.value * 5;
   rateBlock.style.width = `${num}px`;
 }
 point.addEventListener("change", change);
+
+const appendChilder = (daddy, baby) => {
+  return daddy.appendChild(baby);
+};
+
+appendChilder(container, text);
+appendChilder(rateBox, rateBlock);
+appendChilder(container, rateBox);
+appendChilder(rateBox, yellowImage);
+appendChilder(container, point);
+appendChilder(body, container);
